9e4a2ece5d6dc81eedbcc221aa2f4d52894536a0
Markvitim <markvitim@gmail.com>
2022-03-13 12:06:08 +0300

    create test with popup

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
 
@@ -27,4 +26,18 @@ public class CardDeliveryOrderTest {
         $(withText("Успешно!")).shouldBe(Condition.appear, Duration.ofMillis(14999));
     }
 
+    @Test
+    public void shouldSendFormUsePopup() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[placeholder=\"Город\"].input__control").setValue("Мо");
+        $$(".menu-item").find(Condition.text("Москва")).click();
+        $(".input__icon").click();
+        $("[data-day=\"1647723600000\"].calendar__day").click();
+        $("[name=\"name\"].input__control").setValue("Иванов");
+        $("[name=\"phone\"].input__control").setValue("+79334568598");
+        $(".checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно!")).shouldBe(Condition.appear, Duration.ofMillis(14999));
+    }
 }
