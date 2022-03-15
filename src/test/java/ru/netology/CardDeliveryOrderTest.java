ebc7a043ed0bb5d3d361124acda58538beb71134
Markvitim <markvitim@gmail.com>
2022-03-15 13:43:37 +0300

    change tets

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -3,40 +3,55 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
 
+    public static String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
-    public void shouldSendForm() {
+    public void shouldSendFormPlus5Days() {
+        String planingDate = generateDate(5);
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[placeholder=\"Город\"].input__control").setValue("Москва");
-        $("[placeholder=\"Дата встречи\"].input__control").doubleClick();
-        $("[placeholder=\"Дата встречи\"].input__control").sendKeys("17.03.2022");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planingDate);
         $("[name=\"name\"].input__control").setValue("Иванов");
         $("[name=\"phone\"].input__control").setValue("+79334568598");
         $(".checkbox__box").click();
         $(".button__text").click();
-        $(withText("Успешно!")).shouldBe(Condition.appear, Duration.ofMillis(14999));
+        $(withText("Успешно!")).shouldBe(Condition.appear, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на "
+                + planingDate), Duration.ofSeconds(15));
     }
+
 
     @Test
     public void shouldSendFormUsePopup() {
+        String planingDate = generateDate(7);
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[placeholder=\"Город\"].input__control").setValue("Мо");
         $$(".menu-item").find(Condition.text("Москва")).click();
         $(".input__icon").click();
-        $("[data-day=\"1647723600000\"].calendar__day").click();
+        $("[data-day=\"1647896400000\"].calendar__day").click();
         $("[name=\"name\"].input__control").setValue("Иванов");
         $("[name=\"phone\"].input__control").setValue("+79334568598");
         $(".checkbox__box").click();
         $(".button__text").click();
-        $(withText("Успешно!")).shouldBe(Condition.appear, Duration.ofMillis(14999));
+        $(withText("Успешно!")).shouldBe(Condition.appear, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на "
+                + planingDate), Duration.ofSeconds(15));
     }
+
 }
